706cb623c352ff63b9d525675dc4f43826957c49
Bonaldo21 <cauakingg@gmail.com>
2026-04-15 00:28:04 -0300

    Iniciando Estrutura do Projeto

--- src/main/java/com/erp/distruibuidora/ErPdistruibuidoraApplication.java
+++ src/main/java/com/erp/distruibuidora/ErPdistruibuidoraApplication.java
@@ -1,4 +1,4 @@
-package com.example.ERPdistruibuidora;
+package com.erp.distruibuidora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/erp/distruibuidora/ErPdistruibuidoraApplicationTests.java
+++ src/test/java/com/erp/distruibuidora/ErPdistruibuidoraApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.ERPdistruibuidora;
+package com.erp.distruibuidora;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
