fc9bd08d52eaf7fdfd358a1069e9c20a8ca2cff0
Bonaldo21 <cauakingg@gmail.com>
2026-04-15 00:10:30 -0300

    Testando

---
+++ src/main/java/com/example/ERPdistruibuidora/ErPdistruibuidoraApplication.java
@@ -0,0 +1,13 @@
+package com.example.ERPdistruibuidora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErPdistruibuidoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErPdistruibuidoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ERPdistruibuidora/ErPdistruibuidoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ERPdistruibuidora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErPdistruibuidoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
